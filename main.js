58b36de3095ad155cf79ee211bdbb46ed5b2e604
9ta1ls <rpeleh19@gmail.com>
2023-10-03 23:32:44 +0300

    Lab_3

--- main.js
+++ main.js
@@ -2,20 +2,14 @@ const fs = require("fs");
 fs.readFile("data.json", (err,data)=>{
     if(err===null){
         const jsonArr = JSON.parse(data);
-        let index = 0;
         let jsonArrMin = jsonArr.reduce((min, obj)=>(obj.value < min.value ? obj: min),jsonArr[0]);
-
         let jsonStr = jsonArrMin.txt+ ":" + jsonArrMin.value;
        fs.writeFile("output.txt",jsonStr, (err)=>{
-        if(err===null){
-        }
-        else{
+        if(err===null){}
+        else
             console.error('Помилка під час запису у файл:', err);
-        }
        });
     }
-    else{
+    else
         console.log(err);
-    }
-
 })
